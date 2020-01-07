58a2f258a667c10c1ae261f023da38396c0b047e
vitocchi <vitocchi.mm@gmail.com>
2020-01-07 13:52:58 +0900

    判断基準に残り枚数を追加

--- strategy/memorize.go
+++ strategy/memorize.go
@@ -14,7 +14,7 @@ func (s *MemorizeStrategy) DecideFirstTarget(cm card.CardMap, currentTurn card.T
 	list := newPickedMemoryList(cm)
 	mostlikely := list.getMostLikelyMemory()
 	fmt.Printf("most likely memory:%s\n", mostlikely.String())
-	if s.willTryToPickByMemory(mostlikely, currentTurn) {
+	if s.willTryToPickByMemory(mostlikely, currentTurn, len(cm)) {
 		fmt.Println("try to pick")
 		return mostlikely.secondPickedCard
 	}
@@ -25,11 +25,11 @@ func (s *MemorizeStrategy) DecideFirstTarget(cm card.CardMap, currentTurn card.T
 // returns if try to pick memories card.
 // if memory doesnt have two cards memory, it returns false
 // if sum of elapsedTurn of cards memory is more than threshold, it return false
-func (s *MemorizeStrategy) willTryToPickByMemory(pm pickedMemory, currentTurn card.Turn) bool {
+func (s *MemorizeStrategy) willTryToPickByMemory(pm pickedMemory, currentTurn card.Turn, cardNum int) bool {
 	if !pm.haveTwoMemory() {
 		return false
 	}
-	if int(2*currentTurn)-int(pm.turnSum()) > s.TurnThreshold {
+	if int(2*currentTurn)-int(pm.turnSum()) > s.TurnThreshold+52/cardNum {
 		return false
 	}
 	return true
@@ -38,7 +38,7 @@ func (s *MemorizeStrategy) willTryToPickByMemory(pm pickedMemory, currentTurn ca
 func (s *MemorizeStrategy) DecideSecondTarget(cm card.CardMap, currentTurn card.Turn, firstPicked card.Card) card.Card {
 	target := cm.GetMostRecentlyFlippedInNumber(firstPicked.Number())
 	fmt.Printf("most likey card in number:%v picked at %d\n", target, cm[target])
-	if s.willTryToPick(cm[target], currentTurn) {
+	if s.willTryToPick(cm[target], currentTurn, len(cm)) {
 		fmt.Println("try to pick")
 		return target
 	}
@@ -46,9 +46,9 @@ func (s *MemorizeStrategy) DecideSecondTarget(cm card.CardMap, currentTurn card.
 	return cm.GetMostFormalyFlipped()
 }
 
-func (s *MemorizeStrategy) willTryToPick(pickedAt, currentTurn card.Turn) bool {
+func (s *MemorizeStrategy) willTryToPick(pickedAt, currentTurn card.Turn, cardNum int) bool {
 	if pickedAt == 0 {
 		return false
 	}
-	return int(currentTurn)-int(pickedAt) < s.TurnThreshold/2
+	return int(currentTurn)-int(pickedAt) < s.TurnThreshold/2+52/cardNum
 }
